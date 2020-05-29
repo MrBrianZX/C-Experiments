a5ec0f1394f5391c3156029cd00c69c11e141fcf
Brian <brian.soto44@uabc.edu.mx>
2020-05-28 18:02:25 -0700

    creada la biblioteca vector

--- Bibliotecas/math.h
+++ Bibliotecas/math.h
@@ -1,7 +1,58 @@
-int promArray(int array[], int size){
+typedef struct{
+    float array[];
+    float magnitude;
+    float angle;
+    int size;
+} Vector;
+
+/*Operaciones Escalares*/
+int arrayAdd(Vector vectorA){
     int i;
-    int prom = 0;
-    for(i=0; i<size; i++)
-        prom += array[i];
-    return prom;
+    int sum = 0;
+    for(i=0; i<vectorA.size; i++)
+        sum += vectorA.array[i];
+    return sum;
+}
+
+int arraySubtract(Vector vectorA){
+    int i;
+    int sum = 0;
+    for(i=0; i<vectorA.size; i++)
+        sum -= vectorA.array[i];
+    return sum;
+}
+
+int arrayDotProduct(Vector vectorA, Vector vectorB){
+    int sum = 0, i;
+    if(vectorA.size == vectorB.size)
+        for (i = 0; i < vectorA.size; i++)
+            vectorA.array[i] += vectorA.array[i]*vectorB.array[i];
+    return sum;
+}
+
+int promArray(Vector vectorA){
+    return arrayAdd(vectorA)/vectorA.size;
+}
+
+/*Operaciones Vectoriales*/
+void arrayAdd(Vector vectorA, Vector vectorB){
+    int i;
+    if(vectorA.size == vectorB.size)
+        for (i = 0; i < vectorA.size; i++)
+            vectorA.array[i]=vectorA.array[i]+vectorB.array[i];
+}
+
+void arraySustract(Vector vectorA, Vector vectorB){
+    int i;
+    if(vectorA.size == vectorB.size)
+        for (i = 0; i < vectorA.size; i++)
+            vectorA.array[i]=vectorA.array[i]-vectorB.array[i];
+}
+
+
+
+void arrayScalarMultiplication(Vector vectorA, int num){
+    int sum = 0, i;
+    for (i = 0; i < vectorA.size; i++)
+        vectorA.array[i]=num*vectorA.array[i];
 }
