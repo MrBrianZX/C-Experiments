2ef937861cc0bd2d48bad20b910a0810140e42f7
Brian Soto <brian.soto44@uabc.edu.mx>
2021-02-25 11:18:26 -0800

    Mejoras

--- Estructuras de Datos/pila.c
+++ Estructuras de Datos/pila.c
@@ -1,17 +1,32 @@
 #include<stdio.h>
 
-typedef struct
+typedef struct NODO
 {
-    int *siguiente;
+    struct NODO *anterior;
     int dato;
 } NODO;
 
+NODO push(NODO *punto, int dato);
 
 int main(){
+    NODO pila;
+    pila.anterior = NULL;
+
+    printf("%p", pila.anterior);
+    NODO pila = push(&pila, 5);
+
     return 0;
 }
 
-NODO push(NODO *punto){
+NODO push(NODO *punto, int dato){
+    NODO aux;
 
+    aux.anterior = punto;
+    aux.dato = dato;
+
+    return aux;
 }
 
+void printPila(NODO *fin){
+    printf("%d \t %p", fin->dato, fin->anterior)
+}
